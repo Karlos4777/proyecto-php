a65a96ef32c0879c9a51a07dc49641df2f4d55f0
Karlos4777 <karlosz4777@gmail.com>
2025-04-29 17:04:58 -0500

    se agrego un monton de cosas con ctrl + c y crtl + v :D

---
+++ vistas/modulos/inicio.php
@@ -0,0 +1,21 @@
+<div class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <ul class="nav nav-justified py-2 nav-pills">
+                <li class="nav-item">
+                    <a href="index.php?pagina=registro" class="nav-link">Registro</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?pagina=ingreso" class="nav-link">Ingreso</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?pagina=inicio" class="nav-link active">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?pagina=inventario" class="nav-link">Inventario</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?pagina=salir" class="nav-link">Salir</a>
+                </li>
+            </ul>
+        </div>
+    </div>
